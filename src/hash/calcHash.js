01c4b12e46da811ed076a62d3dacb3efc4de346a
Pavel Kizhlo <pavelkizh@gmail.com>
2022-12-02 23:24:41 +0300

    feat: complete "hash" subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
+import { createReadStream } from 'fs';
+import { stdout } from 'node:process';
+
+const { createHash } = await import('node:crypto');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+  const input = createReadStream('./files/fileToCalculateHashFor.txt');
+
+  input.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
