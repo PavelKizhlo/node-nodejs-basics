55944e46a4c270e2d69af97102ca2fae976f6192
Pavel Kizhlo <pavelkizh@gmail.com>
2022-12-04 19:37:37 +0300

    feat: complete "worker threads" subtask

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+  let incrementalNum = 10;
+  const cores = cpus().length;
+  const results = [];
+
+  for (let i = 0; i < cores; i++) {
+    const calculation = new Promise((resolve, reject) => {
+      const worker = new Worker('./worker.js', { workerData: incrementalNum++ });
+      worker.on('message', resolve);
+      worker.on('error', reject);
+    })
+        .then(data => ({
+          status: 'resolved',
+          data
+        }))
+        .catch(err => ({
+          status: 'error',
+          data: null
+        }))
+
+    results.push(calculation);
+  }
+
+  console.log(await Promise.all(results));
 };
 
 await performCalculations();
