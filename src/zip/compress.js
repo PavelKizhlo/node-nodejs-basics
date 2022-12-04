384633cfcb795ef776a9734ef76e8c3244d285bf
Pavel Kizhlo <pavelkizh@gmail.com>
2022-12-04 15:51:10 +0300

    feat: complete "zlib" subtask

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
+const SRC_FILENAME = './files/fileToCompress.txt';
+const DEST_FILENAME = './files/archive.gz';
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    const input = createReadStream(SRC_FILENAME);
+    const gzip = createGzip();
+    const output = createWriteStream(DEST_FILENAME);
+
+    input.pipe(gzip).pipe(output);
+  } catch (err) {
+    throw err;
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+
+const SRC_FILENAME = './files/archive.gz';
+const DEST_FILENAME = './files/fileToCompress.txt';
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    const input = createReadStream(SRC_FILENAME);
+    const gzip = createGunzip();
+    const output = createWriteStream(DEST_FILENAME);
+
+    input.pipe(gzip).pipe(output);
+  } catch (err) {
+    throw err;
+  }
 };
 
 await decompress();
